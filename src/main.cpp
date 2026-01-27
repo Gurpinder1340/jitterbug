aee1a84d147c72903da3006c162dcedae9e373a2
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-01-27 19:48:01 +0000

    loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,45 @@
 #include <bn_core.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
+#include <bn_sprite_ptr.h>
+#include "bn_sprite_items_bug.h"
+#include <bn_random.h>
+#include <bn_vector.h>
+#include <bn_keypad.h>
 
 int main () {
     bn::core::init();
     
-    
+    bn::random rng = bn::random();
+
     bn::backdrop::set_color(bn::color(31, 0, 31));
 
+    bn::vector<bn::sprite_ptr, 50> bugs;
+
+    // bn::sprite_ptr ladybug = bn::sprite_items::bug.create_sprite();
+
     while(true){
+        for(bn::sprite_ptr& ladybug : bugs) {
+        bn::fixed new_x = ladybug.x() + rng.get_fixed(-1,1);
+        bn::fixed new_y = ladybug.y() + rng.get_fixed(-1,1);
         
+        ladybug.set_x(new_x);
+        ladybug.set_y(new_y);
+
+
+        }
+        // bn::fixed new_x = ladybug.x() + rng.get_fixed(-1,1);
+        // bn::fixed new_y = ladybug.y() + rng.get_fixed(-1,1);
+        
+        // ladybug.set_x(new_x);
+        // ladybug.set_y(new_y);
+
+        if(bn::keypad::a_pressed()) {
+            bugs.push_back(bn::sprite_items::bug.create_sprite());
+        }
+
+
+        rng.update();
         bn::core::update();
     }
 }
